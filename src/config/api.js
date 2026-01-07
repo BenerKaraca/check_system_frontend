415d680a7ba7400266ee2cb37bd4f32cc1796aec
BenerKaraca <149328369+BenerKaraca@users.noreply.github.com>
2026-01-07 23:58:23 +0300

    Update api.js

--- src/config/api.js
+++ src/config/api.js
@@ -1,7 +1,4 @@
-const API_BASE_URL = process.env.REACT_APP_API_URL || 'http://localhost:8080';
+// Vercel'deki Environment Variable ismine (REACT_APP_API_BASE_URL) duyarlı hale getirdik
+const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || 'https://check-system-backend.onrender.com';
 
 export default API_BASE_URL;
-
-
-
-
